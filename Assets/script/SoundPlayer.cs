ca3022f94c1741ef5b1c57721cbbcb7ac07d2161
zhw_comany <zhw_comany@sina.com>
2016-12-26 15:13:55 +0800

    tianjia

---
+++ Assets/script/SoundPlayer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SoundPlayer : MonoBehaviour {
+
+	public string userName = "";
+	public string password = "";
+
+	void OnGUI(){
+	/*	AudioSource audio = GetComponent<AudioSource> ();
+		if (GUI.Button (new Rect (200, 500, 100, 50), "开始"))
+			audio.Play ();
+		if (GUI.Button (new Rect (400, 500, 100, 50), "停止"))
+			audio.Stop ();
+		GUI.Label (new Rect (300, 400, 100, 50), "hello world!");
+		*/
+		//登录框
+		GUI.Box (new Rect (10, 10, 200, 120), "登录框");
+		//用户名
+		GUI.Label (new Rect (20, 40, 70, 30), "用户名");
+		userName = GUI.TextField (new Rect (70, 40, 120, 20), userName);
+
+		//密码
+		GUI.Label (new Rect (20, 70, 50, 30), "密码");
+		password = GUI.PasswordField (new Rect (70, 70, 120, 20), password,'*');
+
+		if (GUI.Button (new Rect (70, 100, 50, 25), "登录")) {
+			if (userName == "hellozhw" && password == "1223"){
+					Debug.Log ("登录成功");
+					SceneManager.LoadScene("chapter01_1");
+				}
+			else
+				Debug.Log("登录失败");
+
+		}
+		
+	}
+}
---
+++ Assets/script/Tank.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tank : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		/*float speed = 1;
+
+		if (Input.GetKey(KeyCode.UpArrow))
+		{
+			transform.eulerAngles = new Vector3 (0, 0, 0);
+			transform.position += transform.forward * speed;
+		}
+
+
+		else if (Input.GetKey(KeyCode.DownArrow))
+		{
+			transform.eulerAngles = new Vector3 (0, -180, 0);
+			transform.position += transform.forward * speed;
+		}
+
+		else if (Input.GetKey(KeyCode.LeftArrow))
+		{
+			transform.eulerAngles = new Vector3 (0, 270, 0);
+			transform.position += transform.forward * speed;
+		}
+
+		else if (Input.GetKey(KeyCode.RightArrow))
+		{
+			transform.eulerAngles = new Vector3 (0, 90, 0);
+			transform.position += transform.forward * speed;
+		}
+		*/
+		float steer = 20;
+		float x = Input.GetAxis ("Horizontal");
+		transform.Rotate (0, x * steer * Time.deltaTime, 0);
+
+		//前进后退
+		float speed = 3f;
+		float y = Input.GetAxis ("Vertical");
+		Vector3 s = y * transform.forward * speed * Time.deltaTime;
+		transform.transform.position += s;
+	}
+}
